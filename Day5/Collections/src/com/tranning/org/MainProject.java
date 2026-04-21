51dc03b578c1910627570cd4a4b208b37e84f9fc
Harsh_rode <rodeharsh.0027@gmail.com>
2026-04-21 11:55:39 +0530

    "Traversing with enumrator"

--- Day5/Collections/src/com/tranning/org/MainProject.java
+++ Day5/Collections/src/com/tranning/org/MainProject.java
@@ -1,8 +1,6 @@
 package com.tranning.org;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
+import java.util.*;
 
 public class MainProject {
 
@@ -18,11 +16,21 @@ public class MainProject {
 
         listOfProjects.add(new Project(new Date(),members,"Ajit patil","Ai project"));
 
-        Iterator itr = listOfProjects.iterator();
+//        Iterator itr = listOfProjects.iterator();
+//
+//        while(itr.hasNext())
+//        {
+//            System.out.println(itr.next());
+//        }
 
-        while(itr.hasNext())
-        {
-            System.out.println(itr.next());
+        Vector<String> v = new Vector<String>();
+        v.add("rajesh");
+        v.add("kalpesh");
+        v.add("ramesh");
+
+        Enumeration e = v.elements();
+        while (e.hasMoreElements()){
+            System.out.println(e.nextElement());
         }
     }
 }
